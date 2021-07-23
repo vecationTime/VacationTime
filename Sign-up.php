7dfdd64dcd1feeb760131a6ce7297462f818f34a
Ori-glitch <79941012+Ori-glitch@users.noreply.github.com>
2021-07-23 22:54:03 +0300

    Update Sign-up.php

--- Sign-up.php
+++ Sign-up.php
@@ -1,14 +1,25 @@
 <?php
 
-$username = @$_POST['username'];
-$password = @$_POST['password'];
- 
+$username = $_POST['username'];
+$password = $_POST['password'];
+
 $conn=mysqli_connect("localhost","root","","syber");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
 
+$sql = "SELECT * FROM users WHERE username='".$username."'";
+
+$r = $conn->query($sql);
+
+if ($r->num_rows > 0) {
+  
+  echo "username is already exist";
+
+}
+
+/*
 $sql = "INSERT INTO users (`username`, `password`) VALUES ('$username','$password')";
 
 if ($conn->query($sql) === TRUE) {
@@ -16,9 +27,7 @@ if ($conn->query($sql) === TRUE) {
   } else {
     echo "Error creating table: " . $conn->error;
   }
-  
+ */ 
   $conn->close();
-  
-
 
 ?>
