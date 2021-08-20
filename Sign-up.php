e1857a78d157c8c7d53af4669f63c569ce394ade
sShakedi125 <78536451+sShakedi125@users.noreply.github.com>
2021-08-20 09:10:05 +0300

    Update Sign-up.php

--- Sign-up.php
+++ Sign-up.php
@@ -1,7 +1,19 @@
 <?php
 
+
+
 $username = $_POST['username'];
 $password = $_POST['password'];
+
+if(strlen($password) != 8){
+
+  echo "you need to write 8 charecters";
+  echo "<br><a href='../HTML/Sign-up_Log-in.html'>GO to Home</a>";
+  
+  exit();
+
+}
+
 
 $conn=mysqli_connect("localhost","root","","syber");
 
@@ -16,18 +28,19 @@ $r = $conn->query($sql);
 if ($r->num_rows > 0) {
   
   echo "username is already exist";
+  echo "<br><a href='../HTML/Sign-up_Log-in.html'>GO to Home</a>";
+  exit();
 
 }
 
-/*
-$sql = "INSERT INTO users (`username`, `password`) VALUES ('$username','$password')";
 
+$sql = "INSERT INTO users (`username`, `password`) VALUES ('$username','$password')";
 if ($conn->query($sql) === TRUE) {
     echo "New record is inserted successfully";
   } else {
     echo "Error creating table: " . $conn->error;
   }
- */ 
+ 
   $conn->close();
 
 ?>
