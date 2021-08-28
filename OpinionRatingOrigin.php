8b1e2612cd28d984e2df3a38797eb2f31bbf69f4
sShakedi125 <78536451+sShakedi125@users.noreply.github.com>
2021-08-28 19:15:22 +0300

    Update OpinionRatingOrigin.php

--- OpinionRatingOrigin.php
+++ OpinionRatingOrigin.php
@@ -2,8 +2,8 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Star Rating Form | CodingNepal</title>
-  </head>
+    <title>Star Rating Form</title>
+    <link rel="stylesheet" a href="../CSS/OpinionRating.css">  </head>
   <body>
       <form method="POST" action="../PHP/OpinionRating.php">
         <input type="radio" name="rating" value="1">1
@@ -32,10 +32,42 @@
                 echo "<hr>";
             }
         }
-        
-        
-        
-        
+
         ?>
+<section></section>
+<script >
+          
+          const colors = [
+            '#2196f3',
+            '#e91e63',
+            '#ffeb3b',
+            '#74ff1d'
+          ]
+
+        function createSquare(){
+          const section = document.querySelector('section');
+          const square = document.createElement('span');
+
+          var size = Math.random() * 50;
+
+          square.style.width = 20 + size + 'px';
+          square.style.height = 20 + size + 'px';
+
+          square.style.top = Math.random() * innerHeight + 'px';
+          square.style.left = Math.random() * innerWidth + 'px';
+
+          const bg = colors[Math.floor(Math.random() * colors.length)];
+          square.style.background = bg;
+
+          section.appendChild(square);
+          
+          setTimeout(() => {
+            square.remove()
+          }, 5000)
+        }
+        setInterval(createSquare, 150)
+          </script>-->
+
+
   </body>
 </html>
