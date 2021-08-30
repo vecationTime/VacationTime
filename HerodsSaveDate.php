79f08927c9201f88da8d01d3895a3de9010d8448
Ori-glitch <79941012+Ori-glitch@users.noreply.github.com>
2021-08-31 00:47:10 +0300

    Update and rename SaveDate.php to HerodsSaveDate.php

---
+++ HerodsSaveDate.php
@@ -0,0 +1,25 @@
+<?php
+
+$CheckIn = $_POST['Check-in'];
+$CheckOut = $_POST['Check-out'];
+
+$conn=mysqli_connect("localhost","root","","syber");
+$query = "SELECT * FROM OrderHotel WHERE CheckIn = '$CheckIn' and CheckOut = '$CheckOut'";
+        $result = mysqli_query($conn, $query);
+        $resultCheck = mysqli_num_rows($result);
+        echo "<br>";
+
+        if($resultCheck>0){
+            while($row = mysqli_fetch_assoc($result)){
+               if($row['Available']=="Yes"){
+                header('Location:../HTML/Payment.html');
+               }
+               else{
+                header('Location:../PHP/HerodsDateError.php');
+               }
+              
+            }
+        }
+  $conn->close();
+
+?>
