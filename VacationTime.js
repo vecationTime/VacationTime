906d042cbafc8502d8fad2fcbbd7e548586fb76f
sShakedi125 <78536451+sShakedi125@users.noreply.github.com>
2021-08-31 21:46:27 +0300

    Update VacationTime.js

--- VacationTime.js
+++ VacationTime.js
@@ -1,11 +1,5 @@
-
-// Get the video
 var video = document.getElementById("myVideo");
-
-// Get the button
 var btn = document.getElementById("myBtn");
-
-// Pause and play the video, and change the button text
 function myFunction() {
   if (video.paused) {
     video.play();
@@ -15,7 +9,5 @@ function myFunction() {
     btn.innerHTML = "Play";
   }
 }
-
-
 
 
