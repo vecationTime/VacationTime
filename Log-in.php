6d1212a8079671e40dd94c2affe9ffe10afda1b5
Ori-glitch <79941012+Ori-glitch@users.noreply.github.com>
2021-08-31 13:32:38 +0300

    Update Log-in.php

--- Log-in.php
+++ Log-in.php
@@ -4,18 +4,22 @@ $server = "localhost";
 $username = "root";
 $password = "";
 $db_name = "syber";
-
+session_start();
 $conn = mysqli_connect($server, $username, $password, $db_name);
 $username = $_POST['username'];
 $password = $_POST['password'];
+
+$_SESSION['username']=$username;
+$_SESSION['password']=$password;
 
 $query = "SELECT * FROM users WHERE username = '$username' and passwordP = '$password'";
 
 $result = mysqli_query($conn, $query);
 
 if(mysqli_num_rows($result) == 1){
-    header('Location:../HTML/IsraelCountry.html');
+    header('Location:../PHP/IsraelCountry.php');
 
+   
 }else{
     header('Location:../HTML/Sign-up_Log-in.html');
 }
