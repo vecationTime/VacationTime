e960133c4bc61059d326cb7a28fe7e9b92fe310e
sShakedi125 <78536451+sShakedi125@users.noreply.github.com>
2021-09-01 19:58:58 +0300

    Update Login.php

--- Login.php
+++ Login.php
@@ -9,16 +9,38 @@ $conn = mysqli_connect($server, $username, $password, $db_name);
 $username = $_POST['username'];
 $password = $_POST['password'];
 
+if(strlen($password) != 8){
+
+    echo "you need to write 8 charecters";
+    echo "<br><a href='../HTML/SignUpLogIn.html'>Go back</a>";
+    
+    exit();
+  
+  }
+
 $_SESSION['username']=$username;
 $_SESSION['password']=$password;
 
-$query = "SELECT * FROM users WHERE username = '$username' and passwordP = '$password'";
+$hashed=password_hash($password, PASSWORD_DEFAULT);
+$rep=password_verify($password, $hashed);
+
+$query = "SELECT * FROM users WHERE username = '$username'";
 
 $result = mysqli_query($conn, $query);
 
-if(mysqli_num_rows($result) == 1){
-    header('Location:../PHP/IsraelCountry.php');
-}else{
-    header('Location:../HTML/SignUpLogIn.html');
+$resultCheck = mysqli_num_rows($result);
+
+if($resultCheck>0){
+    while($row = mysqli_fetch_assoc($result)){
+        $rep=password_verify($password, $row['passwordP']);
+        if($rep == 1)
+        {
+            header('Location:../PHP/IsraelCountry.php');
+        }
+        else{
+            header('Location:../HTML/SignUpLogIn.html');
+        }
+    }
 }
+
 ?>
