8fe02fd7e028d89f0c2a969f4b77b9a54de60bfc
sShakedi125 <78536451+sShakedi125@users.noreply.github.com>
2021-09-02 16:11:45 +0300

    Update Payment.php

--- Payment.php
+++ Payment.php
@@ -12,15 +12,23 @@ $LastName = $_POST['LastName'];
 $CardNumber = $_POST['CardNumber'];
 $ExpirationDate = $_POST['ExpirationDate'];
 $CVC = $_POST['CVC'];
+$mail = $_POST['mailo'];
 
-$query = "SELECT * FROM Transactions WHERE id = '$ID' and firstname = '$FirstName'  and lastname = '$LastName' and 
+$query = "SELECT * FROM transections WHERE id = '$ID' and firstname = '$FirstName'  and lastname = '$LastName' and 
 cardnumber = '$CardNumber' and expirationdate = '$ExpirationDate' and cvc = '$CVC'";
 
 $result = mysqli_query($conn, $query);
 
 if(mysqli_num_rows($result) == 1){
     header('Location:../PHP/PaymentS.php');
-
+    $subject  = 'Receipt of the hotel';
+    $message  = 'FirstName: '.$FirstName .'<br> LastName: '.$LastName.'<br> CardNumber: '
+    .$CardNumber.'<br> ExpirationDate: '.$ExpirationDate.'<br> thank you for payment!';
+    $headers  = 'From: [vacationtime2345]@gmail.com' . "\r\n" .
+                'MIME-Version: 1.0' . "\r\n" .
+                'Content-type: text/html; charset=utf-8';
+mail($mail, $subject, $message, $headers);
+header('Location:../PHP/PaymentS.php');
 }else{
     header('Location:../PHP/PaymentError.php');
 }
