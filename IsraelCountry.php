55dbcece8d1d7c99432cff93c9f9d8865c4d3963
sShakedi125 <78536451+sShakedi125@users.noreply.github.com>
2021-09-02 16:25:23 +0300

    Update IsraelCountry.php

--- IsraelCountry.php
+++ IsraelCountry.php
@@ -6,48 +6,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Israel</title>
     <link rel="stylesheet" a href="../CSS/IsraelCountry.css">
-    <style>
-        body
-        {
-            overflow-y: hidden;
-            overflow-x: hidden;
-        }
+     <style>
+        body{ overflow-y: hidden; overflow-x: hidden; }
     </style>
     <script
-   src="https://kit.fontawesome.com/64d58efce2.js"
-   crossorigin="anonymous"
-  ></script>
+   src="https://kit.fontawesome.com/64d58efce2.js"crossorigin="anonymous"></script>
 </head>
 <body>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <a href="../HTML/VacationTime.html"><button class="btn"><i class="fa fa-home"></i></button></a>
-    
     <h1>Click on Israel</h1>
     <div class="container">
-
         <a href="../HTML/IsraelCities.html"><button class="btn-1">Israel</button></a>
-
     </div>
-    
-    <?php
-
-    session_start();
-    echo "<img  id= 'profile' src='../IMG/profile.jpg' style = 'height: 50px; width: 50px; position: relative; 
-    top: -1012px; left: 1590px;'/>";
-    echo "<p style = 'color: white; text-align: center; position: relative; 
-    top: -1100px; left: 800px; font-size: 35px'>".$_SESSION['username']."</p><br>";
-    ?>
-
-<script>
-    var image=document.getElementById("profile");
-    image.addEventListener("mouseover", function(){
-    var container=document.createElement("div");
-    container.innerHTML="Ori";
-    container.style.color= "white"
-    container.style.border="1px solid white";
-    document.body.appendChild(container);    
-    });
-</script>
-
+   <?php
+        session_start();
+        echo "<img src='../IMG/user.png' style='height: 50px; wight:15px; position:relative; 
+        top: -880px; left: 1195px;'/>";
+        echo "<p style = 'color: white; text-align: center; position: relative; 
+        top: -950px; left: 620px; font-size: 25px'> hello ".$_SESSION['username']."</p><br>";
+        ?>
 </body>
 </html>
