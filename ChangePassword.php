a17906b78dee47bbc2ff89779c49d1c90f0c97cd
sShakedi125 <78536451+sShakedi125@users.noreply.github.com>
2021-09-10 21:45:18 +0300

    Update ChangePassword.php

--- ChangePassword.php
+++ ChangePassword.php
@@ -1,17 +1,12 @@
 <?php
-
 $server = "localhost";
 $username = "root";
 $password = "";
 $db_name = "syber";
-
 $conn = mysqli_connect($server, $username, $password, $db_name);
 $mail = $_POST['mail'];
-
 $query = "SELECT * FROM users WHERE mail = '$mail'";
-
 $result = mysqli_query($conn, $query);
-
 if(mysqli_num_rows($result) > 0){
     header('Location:../HTML/UpdatePassword.html');
 }else{
