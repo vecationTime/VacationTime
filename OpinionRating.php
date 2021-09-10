80871c1a1819d44d597098e8fcedf02713f46789
sShakedi125 <78536451+sShakedi125@users.noreply.github.com>
2021-09-11 01:22:32 +0300

    Update OpinionRating.php

--- OpinionRating.php
+++ OpinionRating.php
@@ -1,22 +1,15 @@
 <?php
-
 $Rating = $_POST['rating'];
 $Opinion = $_POST['opinion'];
-
 $conn=mysqli_connect("localhost","root","","syber");
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
   }
-
-
 $sql = "INSERT INTO Opinion (rating, opinion) VALUES ('$Rating','$Opinion')";
-if ($conn->query($sql) === TRUE) {
+if ($conn->query($sql) == TRUE) {
     header('Location: ../PHP/OpinionRatingS.php');
   } else {
     echo "Error creating table: " . $conn->error;
   }
- 
   $conn->close();
-
 ?>
