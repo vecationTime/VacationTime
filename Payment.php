9068463058a1ea64ce74b656bc59331c309c1892
sShakedi125 <78536451+sShakedi125@users.noreply.github.com>
2021-09-11 01:31:41 +0300

    Update Payment.php

--- Payment.php
+++ Payment.php
@@ -1,10 +1,8 @@
 <?php
-
 $server = "localhost";
 $username = "root";
 $password = "";
 $db_name = "syber";
-
 $conn = mysqli_connect($server, $username, $password, $db_name);
 $ID = $_POST['ID'];
 $FirstName = $_POST['FirstName'];
@@ -13,12 +11,9 @@ $CardNumber = $_POST['CardNumber'];
 $ExpirationDate = $_POST['ExpirationDate'];
 $CVC = $_POST['CVC'];
 $mail = $_POST['mailo'];
-
 $query = "SELECT * FROM transections WHERE id = '$ID' and firstname = '$FirstName'  and lastname = '$LastName' and 
-cardnumber = '$CardNumber' and expirationdate = '$ExpirationDate' and cvc = '$CVC'";
-
+cardnumber = '$CardNumber' and expirationdate = '$ExpirationDate' and cvc = '$CVC'"
 $result = mysqli_query($conn, $query);
-
 if(mysqli_num_rows($result) == 1){
     header('Location:../PHP/PaymentS.php');
     $subject  = 'Receipt of the hotel';
