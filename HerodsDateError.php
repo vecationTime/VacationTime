3045d938d0255d82d2c7002bcb75136a856a1b96
sShakedi125 <78536451+sShakedi125@users.noreply.github.com>
2021-09-10 21:46:52 +0300

    Update HerodsDateError.php

--- HerodsDateError.php
+++ HerodsDateError.php
@@ -10,45 +10,30 @@
 </head>
 <body>
     <h1 >Herods Hotel</h1>
-    
     <nav role="navigation">
         <div id="menuToggle">
-         
           <input type="checkbox" />
-       
           <span></span>
           <span></span>
           <span></span>
-        
-       
           <ul id="menu">
             <strong><a href="../HTML/VacationTime.html"><li>home</li></a></strong>
             <strong><a href="../HTML/RentCar.html"><li>rent a car</li></a></strong>
             <strong><a href="../PHP/OpinionRatingOrigin.php"><li>ratingOpinion</li></a></strong>
-            <strong><a href="../HTML/Rooms.html"><li>Rooms</li></a></strong>
             <strong><a href="../Video/Herods Tel Aviv By The Beach, Tel Aviv, Israel.mp4"><li>Virtual tour</li></a></strong>
-
           </ul>
         </div>
       </nav>
-    
-
-      <form style="margin-top: -30px; margin-left: 920px; text-align: center; font-family: 'Poppins', sans-serif;
-      "  method="POST" action="../PHP/HerodsSaveDate.php">
+      <form style="margin-top: 220px; margin-left: 0px; text-align: center; font-family: 'Poppins', sans-serif;
+         "method="POST" action="../PHP/HerodsSaveDate.php">
         <label  style="  color: crimson; font-size: 20px;" for="Check-in:">Check-in</label>
         <input type="date" name="Check-in">
-        
         <label style=" color:crimson; font-size: 20px;" for="Check-out:">Check-out</label>
         <input type="date" name="Check-out">
-        
         <input type="submit" value="search">
-        
-        
+        </form>
         <?php
             echo "<br>"; 
-            echo "<p style = 'color: crimson; text-align: center; font-size: 25px'>The dates aren't available please try again</p><br>";
+            echo "<p style = 'color: crimson; font-size: 25px;text-align:center;'>The dates aren't available please try again</p><br>";
         ?>
-        
-
 </body>
-</html>
