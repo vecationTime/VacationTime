9163decd7d42d6a0d2b3fd8d99be13657e4238e7
sShakedi125 <78536451+sShakedi125@users.noreply.github.com>
2021-09-11 01:23:37 +0300

    Update OpinionRatingOrigin.php

--- OpinionRatingOrigin.php
+++ OpinionRatingOrigin.php
@@ -2,9 +2,10 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Star Rating Form</title>
+    <title>Rating&Opinion Form</title>
     <link rel="stylesheet" a href="../CSS/OpinionRating.css">  </head>
   <body>
+<div class="a">
       <form method="POST" action="../PHP/OpinionRating.php">
         <input type="radio" name="rating" value="1">1
         <input type="radio" name="rating" value="2">2
@@ -16,58 +17,44 @@
           </div>
         <input type="submit" value="post">
         </form>
-        <hr>
-        <hr>
+        </div>
         <?php
         $conn=mysqli_connect("localhost","root","","syber");
         $sql = "SELECT * FROM Opinion;";
         $result = mysqli_query($conn, $sql);
         $resultCheck = mysqli_num_rows($result);
         echo "<br>";
-        echo "<hr>";
         if($resultCheck>0){
             while($row = mysqli_fetch_assoc($result)){
-                echo $row['rating']."-".$row['opinion'];
+                echo "<p style = 'color: white; text-align: center; font-size: 25px';>".$row['rating']."-".$row['opinion'];
                 echo "<br>";
-                echo "<hr>";
             }
         }
-
         ?>
 <section></section>
 <script >
-          
           const colors = [
             '#2196f3',
             '#e91e63',
             '#ffeb3b',
             '#74ff1d'
           ]
-
         function createSquare(){
           const section = document.querySelector('section');
           const square = document.createElement('span');
-
           var size = Math.random() * 50;
-
           square.style.width = 20 + size + 'px';
           square.style.height = 20 + size + 'px';
-
           square.style.top = Math.random() * innerHeight + 'px';
           square.style.left = Math.random() * innerWidth + 'px';
-
           const bg = colors[Math.floor(Math.random() * colors.length)];
           square.style.background = bg;
-
           section.appendChild(square);
-          
           setTimeout(() => {
             square.remove()
           }, 5000)
         }
         setInterval(createSquare, 150)
-          </script>-->
-
-
+          </script>
   </body>
 </html>
