a629703ab4eb0eb68612aa295847a4e076c88221
sShakedi125 <78536451+sShakedi125@users.noreply.github.com>
2021-09-11 00:32:58 +0300

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,5 +1,4 @@
 <?php
-
 $server = "localhost";
 $username = "root";
 $password = "";
@@ -8,28 +7,22 @@ session_start();
 $conn = mysqli_connect($server, $username, $password, $db_name);
 $username = $_POST['username'];
 $password = $_POST['password'];
-
-if(strlen($password) != 8){
-
-    echo "you need to write 8 charecters";
+if(strlen($password) < 8){
+    echo "you need to write 8 or more charecters";
     echo "<br><a href='../HTML/SignUpLogIn.html'>Go back</a>";
-    
     exit();
-  
   }
-
+  $dateh = date("F j, Y, g:i a");
+  $timeh = date("H:i:s");
+  $sql = "INSERT INTO loginhistory (username, dateh, timeh) VALUES ('$username','$dateh','$timeh')";
+  $resulta = mysqli_query($conn, $sql);
 $_SESSION['username']=$username;
 $_SESSION['password']=$password;
-
 $hashed=password_hash($password, PASSWORD_DEFAULT);
 $rep=password_verify($password, $hashed);
-
 $query = "SELECT * FROM users WHERE username = '$username'";
-
 $result = mysqli_query($conn, $query);
-
 $resultCheck = mysqli_num_rows($result);
-
 if($resultCheck>0){
     while($row = mysqli_fetch_assoc($result)){
         $rep=password_verify($password, $row['passwordP']);
@@ -42,5 +35,4 @@ if($resultCheck>0){
         }
     }
 }
-
 ?>
