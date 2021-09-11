8ac817df480cd157acb135b700a47b680b171dcf
Ori-glitch <79941012+Ori-glitch@users.noreply.github.com>
2021-09-11 11:33:04 +0300

    Create LoginHistory.php

---
+++ LoginHistory.php
@@ -0,0 +1,31 @@
+<?php
+
+$username = $_POST['username'];
+$dateh = date("F j, Y, g:i a");
+$timeh = date("H:i:s");
+
+if(strlen($password) != 8){
+
+  echo "you need to write 8 charecters";
+  echo "<br><a href='../HTML/SignUupILogIn.html'>Go back</a>";
+  
+  exit();
+
+}
+
+
+$conn=mysqli_connect("localhost","root","","syber");
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+$sql = "INSERT INTO loginhistory (username, dateh, timeh) VALUES ('$username','$dateh','$timeh')";
+if ($conn->query($sql) == TRUE) {
+    header('Location:../PHP/IsraelCountry.php');
+}else{
+    header('Location:../HTML/SignUpLogIn.html');
+}
+  $conn->close();
+
+?>
