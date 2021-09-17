9a3c9c3ecdcd50986ba9f6cb8eaa5716a90e0d62
sShakedi125 <78536451+sShakedi125@users.noreply.github.com>
2021-09-17 08:49:37 +0300

    Update HerodsSaveDate.php

--- HerodsSaveDate.php
+++ HerodsSaveDate.php
@@ -1,21 +1,26 @@
 <?php
-
 $CheckIn = $_POST['Check-in'];
 $CheckOut = $_POST['Check-out'];
+
 $conn=mysqli_connect("localhost","root","","syber");
+
 $query = "SELECT * FROM OrderHotel WHERE CheckIn = '$CheckIn' and CheckOut = '$CheckOut'";
-        $result = mysqli_query($conn, $query);
-        $resultCheck = mysqli_num_rows($result);
+
+$result = mysqli_query($conn, $query);
+
+$resultCheck = mysqli_num_rows($result);
         echo "<br>";
-        if($resultCheck>0){
-            while($row = mysqli_fetch_assoc($result)){
-               if($row['Available']=="Yes"){
-                header('Location:../HTML/Rooms.html');
-               }
-               else{
-                header('Location:../PHP/HerodsDateError.php');
-               }
-            }
-        }
+if($resultCheck>0){
+  while($row = mysqli_fetch_assoc($result)){
+    if($row['Available']=="Yes")
+    {
+      header('Location:../HTML/Rooms.html');
+    }
+    else
+    {
+      header('Location:../PHP/HerodsDateError.php');
+    }
+  }
+}
   $conn->close();
 ?>
