ac81ea6f6500fa8d42b1fd4f903f3e1592d53692
sShakedi125 <78536451+sShakedi125@users.noreply.github.com>
2021-09-17 09:24:36 +0300

    Update UpdatePassword.php

--- UpdatePassword.php
+++ UpdatePassword.php
@@ -3,13 +3,28 @@ $server = "localhost";
 $username = "root";
 $password = "";
 $db_name = "syber";
+
 $conn = mysqli_connect($server, $username, $password, $db_name);
+
 $username = $_POST['username'];
 $password = $_POST['password'];
-$query = "UPDATE users SET passwordP = '$password' WHERE username = '$username'";
-if(mysqli_query($conn, $query)){
+
+if(strlen($password) < 8)
+{
+    header('Location:../HTML/UpdatePassword.html');
+    exit();
+}
+
+$hashed=password_hash($password, PASSWORD_DEFAULT);
+
+$query = "UPDATE users SET passwordP = '$hashed' WHERE username = '$username'";
+
+if(mysqli_query($conn, $query))
+{
     header('Location:../HTML/SignUpLogIn.html');
-}else{
+}
+else
+{
     header('Location:../HTML/UpdatePassword.html');
 }
 ?>
