05c3dd3458cd6e3499d40ab0cdd112fb51319fa7
sShakedi125 <78536451+sShakedi125@users.noreply.github.com>
2021-09-17 09:05:02 +0300

    Update Messages.php

--- Messages.php
+++ Messages.php
@@ -3,16 +3,26 @@ $server = "localhost";
 $username = "root";
 $password = "";
 $db_name = "syber";
+
 $conn = mysqli_connect($server, $username, $password, $db_name);
+
 $fullname = $_POST['fullname'];
 $email = $_POST['Email'];
 $messages = $_POST['Messages'];
+
 $sql = "INSERT INTO messages (FullName, Email, UserMessages) VALUES ('$fullname','$email','$messages')";
+
 $result = mysqli_query($conn, $query);
-if ($conn->query($sql) == true) {
+
+if ($conn->query($sql) == true) 
+ {
     header('Location:../PHP/MessagesS.php');
- } else {
+ } 
+ else 
+ {
      header('Location:../PHP/MessagesError.php');
  }
+
  $conn->close();
+
 ?>
