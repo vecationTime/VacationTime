3e734de2db7b6491af8e19113a726b622300159e
sShakedi125 <78536451+sShakedi125@users.noreply.github.com>
2021-09-17 09:16:50 +0300

    Update SignUp.php

--- SignUp.php
+++ SignUp.php
@@ -2,24 +2,36 @@
 $username = $_POST['username'];
 $password = $_POST['password'];
 $mail = $_POST['mail'];
-if(strlen($password) < 8){
+
+if(strlen($password) < 8)
+{
   echo "you need to write 8 or more charecters";
   echo "<br><a href='../HTML/SignUpLogIn.html'>Go back</a>";
+
   exit();
 }
+
 $conn=mysqli_connect("localhost","root","","syber");
-if ($conn->connect_error) {
+
+if ($conn->connect_error) 
+{
     die("Connection failed: " . $conn->connect_error);
-  }
+}
+
 $sql = "SELECT * FROM users WHERE username='".$username."'";
 $r = $conn->query($sql);
+
 if ($r->num_rows > 0) {
   echo "username is already exist";
   echo "<br><a href='../HTML/SignUpLogIn.html'>Cant sign up please try again</a>";
+
   exit();
 }
+
 $hashed=password_hash($password, PASSWORD_DEFAULT);
+
 $sql = "INSERT INTO users (username, passwordP, Mail) VALUES ('$username','$hashed','$mail')";
+
 if ($conn->query($sql) == TRUE)
  {
    $subject  = 'Testing sendmail.exe';
@@ -27,12 +39,12 @@ if ($conn->query($sql) == TRUE)
    $headers  = 'From: [vacationtime2345]@gmail.com' . "\r\n" .
             'MIME-Version: 1.0' . "\r\n" .
             'Content-type: text/html; charset=utf-8';
-    mail($mail, $subject, $message, $headers);
-header('Location:../HTML/SignUpLogIn.html');
+             mail($mail, $subject, $message, $headers);
+             header('Location:../HTML/SignUpLogIn.html');
 } 
   else
    {
     echo "Error creating table: " . $conn->error;
-  }
+   }
   $conn->close();
 ?>
