1d83f8103dd5800deb7d3fc6b076b0516ba24198
sShakedi125 <78536451+sShakedi125@users.noreply.github.com>
2021-09-17 08:59:41 +0300

    Update LoginHistory.php

--- LoginHistory.php
+++ LoginHistory.php
@@ -4,23 +4,19 @@ $username = $_POST['username'];
 $dateh = date("F j, Y, g:i a");
 $timeh = date("H:i:s");
 
-if(strlen($password) != 8){
-
+if(strlen($password) < 8){
   echo "you need to write 8 charecters";
   echo "<br><a href='../HTML/SignUupILogIn.html'>Go back</a>";
-  
   exit();
-
 }
-
-
 $conn=mysqli_connect("localhost","root","","syber");
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-  }
+}
 
 $sql = "INSERT INTO loginhistory (username, dateh, timeh) VALUES ('$username','$dateh','$timeh')";
+
 if ($conn->query($sql) == TRUE) {
     header('Location:../PHP/IsraelCountry.php');
 }else{
