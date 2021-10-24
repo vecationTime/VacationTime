5a418616fe6db133d391cc2b4129d3cf75cc06a5
Ori-glitch <79941012+Ori-glitch@users.noreply.github.com>
2021-10-24 19:44:28 +0300

    Update OpinionRating.php

--- OpinionRating.php
+++ OpinionRating.php
@@ -1,21 +1,14 @@
 <?php
 $Rating = $_POST['rating'];
 $Opinion = $_POST['opinion'];
-
 $conn=mysqli_connect("localhost","root","","syber");
-
-if ($conn->connect_error)
- {
+if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
- }
-
+  }
 $sql = "INSERT INTO Opinion (rating, opinion) VALUES ('$Rating','$Opinion')";
-
-if ($conn->query($sql) == TRUE) 
-{
+if ($conn->query($sql) == TRUE) {
     header('Location: ../PHP/OpinionRatingS.php');
-  } 
-  else {
+  } else {
     echo "Error creating table: " . $conn->error;
   }
   $conn->close();
