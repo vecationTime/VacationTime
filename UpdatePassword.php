24cf62c2ecc6f503a5f06cfcf465b5de7afb14df
Ori-glitch <79941012+Ori-glitch@users.noreply.github.com>
2021-10-24 18:48:51 +0300

    Update UpdatePassword.php

--- UpdatePassword.php
+++ UpdatePassword.php
@@ -9,22 +9,16 @@ $conn = mysqli_connect($server, $username, $password, $db_name);
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-if(strlen($password) < 8)
-{
+if(strlen($password) < 8){
     header('Location:../HTML/UpdatePassword.html');
     exit();
-}
-
+  }
+  
 $hashed=password_hash($password, PASSWORD_DEFAULT);
-
 $query = "UPDATE users SET passwordP = '$hashed' WHERE username = '$username'";
-
-if(mysqli_query($conn, $query))
-{
-    header('Location:../HTML/SignUpLogIn.html');
-}
-else
-{
+if(mysqli_query($conn, $query)){
+    header('Location:../HTML/SignUpLogInCust.html');
+}else{
     header('Location:../HTML/UpdatePassword.html');
 }
 ?>
