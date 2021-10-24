38e14a6747b0a46701f472729544d270a4d4fac5
Ori-glitch <79941012+Ori-glitch@users.noreply.github.com>
2021-10-24 19:37:00 +0300

    Create SignUpWork.php

---
+++ SignUpWork.php
@@ -0,0 +1,37 @@
+<?php
+$username = $_POST['username'];
+$password = $_POST['password'];
+$mail = $_POST['mail'];
+if(strlen($password) < 8){
+  echo "you need to write 8 or more charecters";
+  echo "<br><a href='../HTML/SignUpLogIn.html'>Go back</a>";
+  exit();
+}
+$conn=mysqli_connect("localhost","root","","syber");
+if ($conn->connect_error) 
+{
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM workers WHERE username='".$username."'";
+$r = $conn->query($sql);
+
+if ($r->num_rows > 0) {
+  echo "username is already exist";
+  echo "<br><a href='../HTML/SignUpLogIn.html'>Cant sign up please try again</a>";
+  exit();
+}
+
+$hashed=password_hash($password, PASSWORD_DEFAULT);
+$sql = "INSERT INTO workers (username, passwordP, Mail) VALUES ('$username','$hashed','$mail')";
+
+if ($conn->query($sql) == TRUE)
+ {
+    header('Location:../HTML/SignUpLogInWork.html');
+  } 
+  else
+   {
+    echo "Error creating table: " . $conn->error;
+  }
+  $conn->close();
+?>
