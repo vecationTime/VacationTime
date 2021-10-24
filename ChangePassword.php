7293d7a2de2330bc202c75b5122dcc6ee236b5c1
Ori-glitch <79941012+Ori-glitch@users.noreply.github.com>
2021-10-24 18:46:54 +0300

    Update ChangePassword.php

--- ChangePassword.php
+++ ChangePassword.php
@@ -1,16 +1,13 @@
 <?php
-
 $server = "localhost";
 $username = "root";
 $password = "";
-$db_name = "syber";
+$db_name = "syber";   
 
 $conn = mysqli_connect($server, $username, $password, $db_name);
-
 $mail = $_POST['mail'];
 
 $query = "SELECT * FROM users WHERE mail = '$mail'";
-
 $result = mysqli_query($conn, $query);
 
 if(mysqli_num_rows($result) > 0){
