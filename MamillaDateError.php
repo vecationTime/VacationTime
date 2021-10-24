c043f36c6ba304ddf2b22cc81476e347cb1804c0
Ori-glitch <79941012+Ori-glitch@users.noreply.github.com>
2021-10-24 19:29:41 +0300

    Update MamillaDateError.php

--- MamillaDateError.php
+++ MamillaDateError.php
@@ -4,13 +4,6 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../CSS/MamillaHotel.css">
     <title>Mamilla hotel</title>
-    <style>
-      body
-      {
-          overflow-y: hidden;
-          overflow-x: hidden;
-      }
-  </style>
 </head>
 <body>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -30,15 +23,17 @@
           </ul>
         </div>
       </nav>
-       <form style="margin-top: 220px; margin-left: 470px; "  method="POST" action="../PHP/SaveDate.php">
-        <label  style=" color: royalblue; font-size: 20px;" for="Check-in:">Check-in</label>
+      <form style="margin-top: 220px; margin-left: 470px ;font-family: 'Poppins', sans-serif;
+         "method="POST" action="../PHP/HerodsSaveDate.php">
+        <strong><label  style="  color: aqua; font-size: 30px; ;" for="Check-in:">Check-in</label></strong>
         <input type="date" name="Check-in">
-        <label style=" color:royalblue; font-size: 20px;" for="Check-out:">Check-out</label>
+        <strong><label style=" color:aqua; font-size: 30px;" for="Check-out:">Check-out</label></strong>
         <input type="date" name="Check-out">
         <input type="submit" value="search">
+      </form>
         <?php
             echo "<br>"; 
-            echo "<p style = 'color: royalblue; font-size: 25px;margin-left: 15px;'>The dates aren't available please try again</p><br>";
+            echo "<p style = 'color: royalblue; font-size: 25px;'>The dates aren't available please try again</p><br>";
         ?>
 </body>
 </html>
