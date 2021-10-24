181e5130a867689288c530a159ef86184ebada5d
Ori-glitch <79941012+Ori-glitch@users.noreply.github.com>
2021-10-24 19:36:17 +0300

    Update and rename SignUp.php to SignUpCust.php

--- SignUpCust.php
+++ SignUpCust.php
@@ -2,17 +2,12 @@
 $username = $_POST['username'];
 $password = $_POST['password'];
 $mail = $_POST['mail'];
-
-if(strlen($password) < 8)
-{
+if(strlen($password) < 8){
   echo "you need to write 8 or more charecters";
-  echo "<br><a href='../HTML/SignUpLogIn.html'>Go back</a>";
-
+  echo "<br><a href='../HTML/SignUpLogInCust.html'>Go back</a>";
   exit();
 }
-
 $conn=mysqli_connect("localhost","root","","syber");
-
 if ($conn->connect_error) 
 {
     die("Connection failed: " . $conn->connect_error);
@@ -23,13 +18,11 @@ $r = $conn->query($sql);
 
 if ($r->num_rows > 0) {
   echo "username is already exist";
-  echo "<br><a href='../HTML/SignUpLogIn.html'>Cant sign up please try again</a>";
-
+  echo "<br><a href='../HTML/SignUpLogInCust.html'>Cant sign up please try again</a>";
   exit();
 }
 
 $hashed=password_hash($password, PASSWORD_DEFAULT);
-
 $sql = "INSERT INTO users (username, passwordP, Mail) VALUES ('$username','$hashed','$mail')";
 
 if ($conn->query($sql) == TRUE)
@@ -39,12 +32,12 @@ if ($conn->query($sql) == TRUE)
    $headers  = 'From: [vacationtime2345]@gmail.com' . "\r\n" .
             'MIME-Version: 1.0' . "\r\n" .
             'Content-type: text/html; charset=utf-8';
-             mail($mail, $subject, $message, $headers);
-             header('Location:../HTML/SignUpLogIn.html');
-} 
+    mail($mail, $subject, $message, $headers);
+    header('Location:../HTML/SignUpLogInCust.html');
+  } 
   else
    {
     echo "Error creating table: " . $conn->error;
-   }
+  }
   $conn->close();
 ?>
